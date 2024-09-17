b198ad6ff1725b5e98aaac21d5a3729930cb023d
Kahkasha Rafat Fatima <66199148+kahkasha17@users.noreply.github.com>
2024-09-17 10:48:42 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,7 @@ const nextConfig = {
 reactStrictMode:true,
 distDir: 'out',
 output:'export',
-basePath: '/Portfolio', // Replace with your repository name
-assetPrefix: '/Portfolio', // Prefix for static assets
-trailingSlash: true,
+
 };
 
 
