09085acbf041e9f122849538591239e8326cbf31
Kahkasha Rafat Fatima <66199148+kahkasha17@users.noreply.github.com>
2024-09-19 07:53:03 +0530

    manifest

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      {/* <link rel="manifest" href="/site.webmanifest" /> */}
+      <link rel="manifest" href="/site.webmanifest" />
 
       <meta
         name="keywords"
