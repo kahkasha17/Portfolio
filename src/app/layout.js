d602d1aad6cd41cdc1c4d6efb47b18eeeb6fc903
Kahkasha Rafat Fatima <kahkashakhan1417@gmail.com>
2024-09-24 10:58:59 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,22 +11,22 @@ export default function RootLayout({ children }) {
       <link
         rel="icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="favicon-32x32.png"
       />
 
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="favicon-16x16.png"
       />
-      <link rel="manifest" href="/manifest.json" />
+      <link rel="manifest" href="manifest.json" />
 
       <meta
         name="keywords"
